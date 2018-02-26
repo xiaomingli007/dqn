2aefe8d84a0ca6dbbf457dd520994208c5ac3735
逆沙 <nisha.lxm@alibaba-inc.com>
2018-02-26 10:10:39 +0800

    jump update.

---
+++ weichart_jump/game_state.py
@@ -0,0 +1,118 @@
+# -*- coding:utf-8 -*-
+
+import cv2
+import wda
+import numpy as np
+import time
+import os
+np.set_printoptions(threshold=np.nan)
+
+
+
+SEC_PER_DU = 0.11
+
+class gameState:
+    def __init__(self):
+        self.client = wda.Client('http://192.168.1.103:8100')
+        self.sess = self.client.session()
+    def find_head(self,img):
+        # find small circles
+        img_tmp = img.copy()
+        img_tmp = cv2.resize(img_tmp,(540,960))
+        head_found = False
+        trial_cnt = 0
+        while (head_found == False):
+            # processing img
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+            closed = cv2.morphologyEx(img_tmp, cv2.MORPH_CLOSE, kernel)
+            edges = cv2.Canny(closed, 200, 200)  # use full colour img for canny
+            circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 2, 180 - trial_cnt * 10,
+                                       param1=100, param2=65, minRadius=12, maxRadius=18)
+            gray = cv2.cvtColor(closed, cv2.COLOR_RGB2GRAY)
+
+            # if nothing found, will return None, then try a few time.
+            if (circles is not None):
+                head_found = True
+            else:
+                if (trial_cnt > 5):  # return 0 as error.
+                    return 0, 0, 0
+            trial_cnt += 1
+
+        circles = np.uint16(np.around(circles))
+
+        # circle to return
+        xo = 0
+        yo = 0
+        ro = 0
+
+        for i in circles[0, :]:
+            x = i[0]
+            y = i[1]
+            r = i[2]
+
+            # find the head
+            if (r < 25 and y > 380 and y < 520):
+
+                if ((y < yo or yo is 0)):  # 寻找位置最靠上的那个源（有时候身子也会被识别成圆）
+                    xo = x
+                    yo = y
+                    ro = r
+        print '(%s,%s,%s)'%(xo,yo,ro)
+        return xo, yo, ro
+
+    def get_screenshot(self):
+        self.client.screenshot('image/jump_tmp.png')
+
+
+    def start(self,x=207,y=609):
+        terminal = False
+        reward = 1
+        print 'x=%s,y=%s'%(x,y)
+        self.sess.tap(x=x, y=y)
+        img = cv2.imread('image/jump_tmp.png')
+        # print img.shape
+        pos = self.find_head(img)
+        if max(pos) == 0:
+            terminal = True
+            reward = 0
+
+        x_t = cv2.cvtColor(cv2.resize(img, (84, 84)), cv2.COLOR_BGR2GRAY)
+        s_t = x_t[:, :, np.newaxis]
+
+        return s_t, reward, terminal
+
+    def jump_step(self,input_actions):
+
+        terminal = False
+        reward = 1
+        action_index = np.argmax(input_actions)
+
+        tap_time = SEC_PER_DU*(action_index+1)
+
+        img_start = cv2.imread('image/jump_tmp.png')
+        start_pos = self.find_head(img_start)
+        if max(start_pos) == 0:
+            self.start()
+
+        print 'action_index=%s,tap_time=%s'%(action_index,tap_time)
+        self.sess.tap_hold(x=207,y=609,duration=tap_time)
+        time.sleep(3)
+        self.get_screenshot()
+        img = cv2.imread('image/jump_tmp.png')
+        # print img
+
+        pos=self.find_head(img)
+        if max(pos) == 0:
+            terminal = True
+            reward = -1
+
+        x_t = cv2.cvtColor(cv2.resize(img, (84, 84)), cv2.COLOR_BGR2GRAY)
+        s_t = x_t[:, :, np.newaxis]
+
+        return s_t,reward,terminal
+
+
+
+
+
+
--- weichart_jump/jump_dqn.py
+++ weichart_jump/jump_dqn.py
@@ -1,25 +1,202 @@
 import tensorflow as tf
 import cv2
 import sys
-sys.path.append("image/")
-import wrapped_flappy_bird as
 import random
 import numpy as np
 from collections import deque
+import time
+import game_state as game
+
 
 
 GAME = 'jump'
+ACTIONS = 10 # number of valid actions
+GAMMA = 0.99  # decay rate of past observations
+OBSERVE = 1000. # timesteps to observe before training
+EXPLORE = 2000. # frames over which to anneal epsilon
+FINAL_EPSILON = 0.01 # final value of epsilon
+INITIAL_EPSILON = 0.1  # starting value of epsilon
+REPLAY_MEMORY = 50000 # number of previous transitions to remember
+BATCH_SIZE = 32 # size of minibatch
 
 
 
+def buildNetwork():
+
+    #input
+    s = tf.placeholder("float",[None,84,84,1])
+
+    #hidden layer1
+    W_conv1 = weight_variable([8,8,1,32])
+    b_conv1 = bias_variable([32])
+    h_1 = tf.nn.relu(conv2d(s,W_conv1,4)+b_conv1)
+
+    #hidden layer2
+    W_conv2 = weight_variable([4,4,32,64])
+    b_conv2 = bias_variable([64])
+    h_2 = tf.nn.relu(conv2d(h_1,W_conv2,2)+b_conv2)
+
+    #hidden layer3
+    W_conv3 = weight_variable([3,3,64,64])
+    b_conv3 = bias_variable([64])
+    h_3 = tf.nn.relu(conv2d(h_2,W_conv3,1) + b_conv3)
+
+    h_3_flat = tf.reshape(h_3,[-1,7744])
+
+    #full connect layer1
+    W_fc1 = weight_variable([7744,512])
+    b_fc1 = bias_variable([512])
+    fc1 = tf.nn.relu(tf.matmul(h_3_flat,W_fc1) + b_fc1)
+
+    #full connect layer2
+    W_fc2 = weight_variable([512,ACTIONS])
+    b_fc2 = bias_variable([ACTIONS])
+    dqn = tf.matmul(fc1,W_fc2) + b_fc2
 
 
 
+    return s,dqn
+
+
+def train(s,dqn,sess):
+
+    #define loss function
+    a = tf.placeholder("float",[None,ACTIONS])
+    y = tf.placeholder("float",[None])
+    y_ = tf.reduce_sum(tf.multiply(dqn,a),reduction_indices=1)
+    loss = tf.reduce_mean(tf.square(y-y_))
+
+    train_step = tf.train.AdamOptimizer(0.0001).minimize(loss)
+
+    game_state = game.gameState()
+
+    #replay memory
+    D = deque()
 
 
 
+    s_t,r_0,terminal = game_state.start()
+
+
+    # saving and loading network
+    saver = tf.train.Saver()
+    sess.run(tf.initialize_all_variables())
+    checkpoint = tf.train.get_checkpoint_state("saved_networks")
+    if checkpoint and checkpoint.model_checkpoint_path:
+        saver.restore(sess,checkpoint.model_checkpoint_path)
+        print("Successfully loaded:", checkpoint.model_checkpoint_path)
+    else:
+        print("Could not find old network weights")
+
+
+    epsilon = INITIAL_EPSILON
+    t = 0
+    while True:
+    # choose action with epsilon greedy
+        dqn_t = dqn.eval(feed_dict={s:[s_t]})[0]
+        action_t = np.zeros([ACTIONS])
+        action_index = 0
+
+        if random.random() < epsilon:
+            print("----------Random Action----------")
+            action_index = random.randrange(ACTIONS)
+            action_t[action_index] = 1
+        else:
+            action_index = np.argmax(dqn_t)
+            action_t[action_index] = 1
+
+
+        # linearly annealed
+        if epsilon > FINAL_EPSILON and t > OBSERVE:
+            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON)/EXPLORE
+
+        #run the selected action and observe next state and reward
+
+        s_t1,r_t,terminal = game_state.jump_step(action_t)
+
+
+        #store the transition in D
+        D.append((s_t,action_t,r_t,s_t1,terminal))
+
+        if len(D) > REPLAY_MEMORY:
+            D.popleft()
+
+        # only train if done obseving
+        if t > OBSERVE:
+            #sampe a minibath to train on
+            batch = random.sample(D,BATCH_SIZE)
+            #get batch variable
+            s_j_batch = [d[0] for d in batch]
+            a_batch = [d[1] for d in batch]
+            r_batch = [d[2] for d in batch]
+            s_j1_batch = [d[3] for d in batch]
+
+            y_batch = []
+            dqn_j1_batch = dqn.eval(feed_dict={s:s_j1_batch})
+            for i in range(0,len(batch)):
+                terminal = batch[i][4]
+                if terminal:
+                    y_batch.append(r_batch[i])
+                else:
+                    y_batch.append(r_batch[i] + GAMMA*np.max(dqn_j1_batch[i]))
+
+            #sgd
+            train_step.run(feed_dict={
+                y: y_batch,
+                a: a_batch,
+                s: s_j_batch
+            })
+
+
+        s_t = s_t1
+        t += 1
+
+        #save progress every 1000 iteration
+
+        if t%1000 == 0:
+            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
+
+        # print info
+        state = ""
+        if t <= OBSERVE:
+            state = "observe"
+        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
+            state = "explore"
+        else:
+            state = "train"
+
+        print("TIMESTEP=", t, " STATE=", state, \
+              " EPSILON=", epsilon, " ACTION=", action_index, " REWARD=", r_t, \
+              " Q_MAX=%e" % np.max(dqn_t))
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,stddev=0.01)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.01,shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x,W,stride):
+    return  tf.nn.conv2d(x,W,strides=[1,stride,stride,1],padding='SAME')
+
+def max_poll_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
 
 
 
-def wieght_variable(shape):
-    initial = tf.truncated_normal(shape,0.01)
+def playGame():
+    sess = tf.InteractiveSession()
+    s,dqn= buildNetwork()
+    train(s,dqn,sess)
+
+
+def main():
+    playGame()
+
+
+if __name__ == '__main__':
+    main()
+
+
